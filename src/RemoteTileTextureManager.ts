fa8338c521d722a72d9790b45c576158eae5a82e
jonathanlurie <lurie.jo@gmail.com>
2025-11-27 15:43:01 +0100

    check if texture is already being processed

--- src/RemoteTileTextureManager.ts
+++ src/RemoteTileTextureManager.ts
@@ -11,17 +11,18 @@ export class RemoteTileTextureManager {
   private readonly texturePool: QuickLRU<string, Texture>;
   private readonly unavailableTextures = new Set();
   private readonly textureLoader = new TextureLoader();
+  private readonly textureInProgress = new Map<string, Texture>();
 
 
   constructor(options: TextureManagerOptions = {}) {
-    const cacheSize = options.cacheSize ?? 1000;
+    const cacheSize = options.cacheSize ?? 10000;
 
     this.texturePool = new QuickLRU<string, Texture>({
       // should be replaced by gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS);
       maxSize: cacheSize,
   
       onEviction(_key: string, value: Texture) {
-        console.log("freeing texture GPU memory");
+        console.log("Freeing texture from GPU memory");
         value.dispose();
       }
     });
@@ -39,32 +40,48 @@ export class RemoteTileTextureManager {
         .replace("{y}", tileIndexWrapped.y.toString())
         .replace("{z}", tileIndexWrapped.z.toString());
 
+      // The texture is not existing. An unfruitful attempt was made already
       if (this.unavailableTextures.has(textureURL)) {
         return reject(new Error("Could not load texture."));
       }
 
+      // The texture is in the pool of already fetched textures
       if (this.texturePool.has(textureURL)) {        
         resolve(this.texturePool.get(textureURL) as Texture);
-      } else {
-        this.textureLoader.load(
-          textureURL,
-          
-          ( texture ) => {
-            texture.flipY = false;
-            this.texturePool.set(textureURL, texture);            
-            resolve(texture);
-          },
-        
-          // onProgress callback currently not supported
-          undefined,
-        
-          // onError callback
-          ( err ) => {
-            this.unavailableTextures.add(textureURL);
-            reject(new Error("Could not load texture."))
-          }
-        );      
+        return;
       }
+
+      // A request of this texture has already been made but is not finished yet
+      if (this.textureInProgress.has(textureURL)) {
+        resolve(this.textureInProgress.get(textureURL) as Texture);
+        return;
+      }
+
+      const tempTexture = this.textureLoader.load(
+        textureURL,
+        
+        ( texture ) => {
+          texture.flipY = false;
+          this.texturePool.set(textureURL, texture);    
+          this.textureInProgress.delete(textureURL);        
+          resolve(texture);
+        },
+      
+        // onProgress callback currently not supported
+        undefined,
+      
+        // onError callback
+        ( err ) => {
+          this.unavailableTextures.add(textureURL);
+
+          this.textureInProgress.delete(textureURL);
+
+          reject(new Error("Could not load texture."))
+        }
+      );
+
+      this.textureInProgress.set(textureURL, tempTexture);
+    
     });
   }
 
